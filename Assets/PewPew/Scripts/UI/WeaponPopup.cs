4494a6ac3039cdb5ac71a541e8aa420cc9892cee
scora-off <scora.lal@gmail.com>
2026-05-24 15:46:24 +0300

    UI, Arena, etc

---
+++ Assets/PewPew/Scripts/UI/WeaponPopup.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPopup : MonoBehaviour
+{
+	[SerializeField] private Button closeButton;
+
+	private void Start()
+	{
+		closeButton.onClick.AddListener(Close);
+	}
+
+	private void Close()
+	{
+		gameObject.SetActive(false);
+	}
+}
